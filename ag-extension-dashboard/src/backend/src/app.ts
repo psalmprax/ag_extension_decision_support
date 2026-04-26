f660521065f81de3cdb88f0f2f6b0cafcaf375e0
dollegit <dollegit@yahoo.com>
2026-04-26 15:30:42 +0200

    Troubleshoot: Disable timeout middleware

--- ag-extension-dashboard/src/backend/src/app.ts
+++ ag-extension-dashboard/src/backend/src/app.ts
@@ -78,6 +78,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(limiter);
 
+/*
 // Request timeout middleware - 300s timeout for all requests during AI stabilization (Slow local LLM)
 app.use((req, res, next) => {
     res.setTimeout(300000, () => {
@@ -88,6 +89,7 @@ app.use((req, res, next) => {
     });
     next();
 });
+*/
 
 app.use(securityGate);
 
